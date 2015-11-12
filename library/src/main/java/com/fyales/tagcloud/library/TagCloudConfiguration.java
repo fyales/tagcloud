a5893cb7ad7d823732d61665c8b8870abace3226
fyales <sww201006@126.com>
2015-11-12 17:56:51 +0800

    nothing

--- library/src/main/java/com/fyales/tagcloud/library/TagCloudConfiguration.java
+++ library/src/main/java/com/fyales/tagcloud/library/TagCloudConfiguration.java
@@ -12,15 +12,20 @@ public class TagCloudConfiguration {
 
     private static final int DEFAULT_LINE_SPACING = 5;
     private static final int DEFAULT_TAG_SPACING = 10;
+    private static final int DEFAULT_FIXED_COLUMN_SIZE = 3; //默认列数
 
     private int lineSpacing;
     private int tagSpacing;
+    private int columnSize;
+    private boolean isFixed;
 
    public TagCloudConfiguration(Context context,AttributeSet attrs){
        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TagCloudLayout);
        try {
            lineSpacing = a.getDimensionPixelSize(R.styleable.TagCloudLayout_lineSpacing, DEFAULT_LINE_SPACING);
            tagSpacing = a.getDimensionPixelSize(R.styleable.TagCloudLayout_tagSpacing, DEFAULT_TAG_SPACING);
+           columnSize = a.getInteger(R.styleable.TagCloudLayout_columnSize, DEFAULT_FIXED_COLUMN_SIZE);
+           isFixed = a.getBoolean(R.styleable.TagCloudLayout_isFixed,false);
        } finally {
            a.recycle();
        }
@@ -40,5 +45,21 @@ public class TagCloudConfiguration {
 
     public void setTagSpacing(int tagSpacing) {
         this.tagSpacing = tagSpacing;
+    }
+
+    public int getColumnSize() {
+        return columnSize;
+    }
+
+    public void setColumnSize(int columnSize) {
+        this.columnSize = columnSize;
+    }
+
+    public boolean isFixed() {
+        return isFixed;
+    }
+
+    public void setIsFixed(boolean isFixed) {
+        this.isFixed = isFixed;
     }
 }
